e135413f2734787819cae2b09062fd44707ba969
@viniciuswqi <158781792+Mvini7@users.noreply.github.com>
2024-05-06 18:52:34 -0300

    versão 0.0.1
    Atividade do Marcos Vinicius (Aula 1 de Java Script).

---
+++ AULA 1 - JS/aula1.js
@@ -0,0 +1,31 @@
+let licya = [
+  `burro`,
+  `macaco`,
+  15,
+  `coca`,
+  {
+    name: "Pedrão",
+    age: 15,
+    isAdm: true
+  },
+  true
+]
+
+console.log(`O seu nome é ${licya[4].name}, seu ${licya[0]}`)
+
+
+
+
+
+// var name = prompt("Qual o seu nome?")
+// var age = prompt("Qual sua idade?")
+
+
+
+// let joao = {
+//   name: name,
+//   age: age,
+//   isHumam: true
+// }
+
+// console.log (`Olá, ${joao.name}, você tem ${joao.age} anos.`)
