ce4d5c58d3fb45859c636fef1233cd9d6badb16c
@viniciuswqi <158781792+Mvini7@users.noreply.github.com>
2024-05-13 18:01:35 -0300

    Add files via upload

---
+++ AULA 1/atv02/AULA 1/atv02/index.js
@@ -0,0 +1,43 @@
+// var name = 'Vinicius'
+
+// let dodge = {
+//   pneu: "borracha",
+//   padrão: 'Completa',
+//   completa: true
+// }
+
+// const minhalista = [
+//   name,
+//   dodge,
+//     {
+//       animals: 'cat' ,
+//       age: 3
+//     },
+//     "monkey"
+// ]
+
+// console.log(dodge.completa)
+
+// let media_escolar = 1
+
+// if (media_escolar >= 8){
+//   console.log('Aprovado')
+// }
+
+// else if (media_escolar < 8 && media_escolar >=6){
+//   console.log('Recuperação')
+// }
+// else {
+//   console.log('Reprovado')
+// }
+
+var numero = prompt1 ('Digite um numero')
+
+var numero = numero % 2 === 0
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const numerosPares = numeros. filter(numero => numero % 2 === 0);
+
+console.log(numerosPares)
+
